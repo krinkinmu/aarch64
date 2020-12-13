ed38cf2f582d1c45a883d583882f5c93b28dec5b
Mike Krinkin <krinkin.m.u@gmail.com>
2020-12-13 19:57:34 +0000

    Add a comment to the Rust code about HiKey960 PL011 parameters
    There is a similar comment in the C code and it's quite convenient
    to have in the code when building it for both QEMU and HiKey960.

--- kernel/start/src/lib.rs
+++ kernel/start/src/lib.rs
@@ -4,6 +4,12 @@ use pl011::PL011;
 
 #[no_mangle]
 pub extern "C" fn start_kernel() {
+    // For HiKey960 board that I have the following parameters were found to
+    // work fine:
+    //
+    // let serial = PL011::new(
+    //     /* base_address = */0xfff32000,
+    //     /* base_clock = */19200000);
     let serial = PL011::new(
         /* base_address = */0x9000000,
         /* base_clock = */24000000);
