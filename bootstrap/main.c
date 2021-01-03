5047ffd8ed9ebac6ef0b059269f34a34bbc57f40
Mike Krinkin <krinkin.m.u@gmail.com>
2021-01-03 16:49:12 +0000

    Introduce basic interrupt handling logic
    The current logic just calls into a C routine (doing all the normal
    stuff like preserving GPRs and so on) and doesn't actually handle
    anything. Moreover as of now we don't even provide enough information
    to the handler to understand the cause to begin with.
    
    However the current logic is enough to verify that our
    interrupt/exception setup works in general and that the GPRs and other
    registers we want to preserve are preserved across the interrupt
    handlers. So that's good.
    
    I tested the logic by generating a data about by dereferencing an
    incorrect pointer and then comparing the values in the registers before
    and after the handler using QEMU monitor (info registers) and GDB (to
    set breakpoints).

--- bootstrap/main.c
+++ bootstrap/main.c
@@ -6,6 +6,31 @@
 #include "pl011.h"
 
 
+struct stack_frame {
+    uint64_t x0;
+    uint64_t x1;
+    uint64_t x2;
+    uint64_t x3;
+    uint64_t x4;
+    uint64_t x5;
+    uint64_t x6;
+    uint64_t x7;
+    uint64_t x8;
+    uint64_t x9;
+    uint64_t x10;
+    uint64_t x11;
+    uint64_t x12;
+    uint64_t x13;
+    uint64_t x14;
+    uint64_t x15;
+    uint64_t x16;
+    uint64_t x17;
+    uint64_t x18;
+    uint64_t fp;
+    uint64_t lr;
+    uint64_t sp;
+};
+
 // We get a list of these from the UEFI bootloader. It describes modules that
 // the bootloader loaded in memory for us. It's up to the bootstrap code to
 // convert this list into struct boot_info below that the kernel expects.
@@ -39,6 +64,14 @@ static void reserve_ranges(const struct data *data, size_t size)
     }
 }
 
+static struct pl011 *logger;
+
+void interrupt_handler(struct stack_frame *frame)
+{
+    if (logger)
+        pl011_send(logger, "Interrupt\n", sizeof("Interrupt\n"));
+}
+
 void start_kernel(void);
 
 // I'm using this cont variable to pause the kernel early during the boot
@@ -63,6 +96,10 @@ void main(struct data *data, size_t size)
     pl011_setup(
         &serial, /* base_address = */0x9000000, /* base_clock = */24000000);
     pl011_send(&serial, "Bootstraping...\n", sizeof("Bootstraping...\n"));
+
+    // Uncomment to generate an exception to test the interrupt/exception setup
+    // logger = &serial;
+    // cont = *((volatile int *)0xffffffffffffffffull);
 
     bootstrap_allocator_setup();
     reserve_ranges(data, size);
