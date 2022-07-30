7380a45fc904b8a70bb7bd9d59a3de0e6b90c37b
Mike Krinkin <krinkin.m.u@gmail.com>
2022-07-30 15:23:03 +0100

    Add memset function to the C library

--- c/string.c
+++ c/string.c
@@ -5,3 +5,9 @@ size_t strlen(const char* str) {
     while (*str) str++;
     return str - b;
 }
+
+void* memset(void* dst, int c, size_t n) {
+    char* s = (char *)dst;
+    for (size_t i = 0; i < n; ++i) s[i] = c;
+    return dst;
+}
--- c/string.h
+++ c/string.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 
 size_t strlen(const char* str);
+void* memset(void* dst, int c, size_t n);
 
 #endif  // __C_STRING_H__
